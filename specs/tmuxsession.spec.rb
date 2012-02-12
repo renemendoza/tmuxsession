bc740473e72905b31ae20c6aa306307a2c577d7a
Rene Mendoza <latiendamac@gmail.com>
2012-02-11 18:55:43 -0600

    initial commit

---
+++ specs/tmuxsession.spec.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+#
+#require 'rspec'
+
+#ENV["RAILS_ENV"] ||= 'test'
+#require File.expand_path("../../config/environment", __FILE__)
+#Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+#
